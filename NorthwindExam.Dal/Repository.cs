5ecff037551f4c8cda790fd4204ab14e50af4be0
Nickolai Schioldan Winther <nick3846@edu.campusvejle.dk>
2019-09-16 10:31:05 +0200

    Add project files.

---
+++ NorthwindExam.Dal/Repository.cs
@@ -0,0 +1,32 @@
+﻿using NorthwindExam.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindExam.Dal
+{
+    public class Repository
+    {
+        public NorthwindModel model = new NorthwindModel();
+
+        public List<Order> GetAllOrders()
+        {
+            return model.Orders.ToList();
+        }
+
+        public void Update(Order order)
+        {
+            model.Orders.AddOrUpdate(order);
+            model.SaveChanges();
+        }
+
+        public void Insert(Order order)
+        {
+            model.Orders.Add(order);
+            model.SaveChanges();
+        }
+    }
+}
---
+++ NorthwindExam.Gui.Desktop/UserControls/StackedButtonNavigationUserControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NorthwindExam.Gui.Desktop.UserControls
+{
+    /// <summary>
+    /// Interaction logic for StackedButtonNavigationUserControl.xaml
+    /// </summary>
+    public partial class StackedButtonNavigationUserControl : UserControl
+    {
+        MainWindow mainWindow;
+        public StackedButtonNavigationUserControl(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+
+        private void ToggleButtonOrder_Checked(object sender, RoutedEventArgs e)
+        {
+            if (((ToggleButton)sender).IsChecked ?? false)
+            {
+                mainWindow.DetailsUserControl.Content = new OrderUserControl();
+            }
+            else
+            {
+                mainWindow.DetailsUserControl.Content = new UserControl();
+            }
+        }
+    }
+}
